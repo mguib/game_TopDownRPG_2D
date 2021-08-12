77446805432a74ff99d9056c8f69bba01f63ccfc
Marcos Guibson <43528733+mguib@users.noreply.github.com>
2021-08-12 14:39:47 -0400

    feat: Player - Adicionando a função de correr

--- Game RPG TopDown 2D/Assets/Scripts/PlayerAnim.cs
+++ Game RPG TopDown 2D/Assets/Scripts/PlayerAnim.cs
@@ -18,7 +18,17 @@ public class PlayerAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(player.direction.sqrMagnitude > 0)
+        OnMove();
+
+        OnRun();
+        
+    }
+
+    #region Movement
+
+    void OnMove()
+    {
+        if (player.direction.sqrMagnitude > 0)
         {
             anim.SetInteger("transition", 1);
         }
@@ -27,15 +37,24 @@ public class PlayerAnim : MonoBehaviour
             anim.SetInteger("transition", 0);
         }
 
-        if(player.direction.x > 0)
+        if (player.direction.x > 0)
         {
             transform.eulerAngles = new Vector2(0, 0);
         }
 
-        if(player.direction.x < 0)
+        if (player.direction.x < 0)
         {
             transform.eulerAngles = new Vector2(0, 180);
         }
-        
     }
+
+    void OnRun()
+    {
+        if (player.isRunning)
+        {
+            anim.SetInteger("transition", 2);
+        }
+    }
+
+    #endregion
 }
